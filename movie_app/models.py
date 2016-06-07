1b7dda2cb1780e9f5d322fec3cf6cc28ef599307
hpaasch <hepaasch@gmail.com>
2016-06-07 14:12:11 -0400

    Schema migration drafted.

--- movie_app/models.py
+++ movie_app/models.py
@@ -4,10 +4,50 @@ from django.db import models
 
 
 class Movies(models.Model):
-    pass
+    movie_id = models.IntegerField()
+    movie_title = models.CharField(max_length=75)
+    release_date = models.CharField(max_length=15)
+    video_release_date = models.CharField(max_length=15, default="")
+    imdb_url = models.CharField(max_length=300)
+    unknown = models.IntegerField()
+    action = models.IntegerField()
+    adventure = models.IntegerField()
+    animation = models.IntegerField()
+    children = models.IntegerField()
+    comedy = models.IntegerField()
+    crime = models.IntegerField()
+    documentary = models.IntegerField()
+    drama = models.IntegerField()
+    fantasy = models.IntegerField()
+    film_noir = models.IntegerField()
+    horror = models.IntegerField()
+    musical = models.IntegerField()
+    mystery = models.IntegerField()
+    romance = models.IntegerField()
+    scifi = models.IntegerField()
+    thriller = models.IntegerField()
+    war = models.IntegerField()
+    western = models.IntegerField()
+
+    def __str__(self):
+        return self.movie_title
+
 
 class Reviewers(models.Model):
-    pass
+    user_id = models.IntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10, null=True)
+    occupation = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.occupation
+
 
 class Ratings(models.Model):
-    pass
+    user_id = models.ForeignKey(Reviewers)
+    item_id = models.ForeignKey(Movies)
+    rating = models.IntegerField()
+    timestamp = models.IntegerField()
+
+    def __str__(self):
+        return self.rating
