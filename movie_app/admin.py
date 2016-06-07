675dbdfaa386d23715a7079085288a4d5dd36ebb
hpaasch <hepaasch@gmail.com>
2016-06-07 15:37:02 -0400

    Schema Migration repaired. Data import started.

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from movie_app.models import Rating, Rater, Movie
+
+admin.site.register(Rating)
+admin.site.register(Rater)
+admin.site.register(Movie)
--- movie_app/models.py
+++ movie_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Movies(models.Model):
+class Movie(models.Model):
     movie_id = models.IntegerField()
     movie_title = models.CharField(max_length=75)
     release_date = models.CharField(max_length=15)
@@ -33,19 +33,20 @@ class Movies(models.Model):
         return self.movie_title
 
 
-class Reviewers(models.Model):
+class Rater(models.Model):
     user_id = models.IntegerField()
     age = models.IntegerField()
-    gender = models.CharField(max_length=10, null=True)
+    gender = models.CharField(max_length=3, null=True)
     occupation = models.CharField(max_length=15)
+    zip_code = models.CharField(max_length=10)
 
     def __str__(self):
         return self.occupation
 
 
-class Ratings(models.Model):
-    user_id = models.ForeignKey(Reviewers)
-    item_id = models.ForeignKey(Movies)
+class Rating(models.Model):
+    user_id = models.ForeignKey(Rater)
+    item_id = models.ForeignKey(Movie)
     rating = models.IntegerField()
     timestamp = models.IntegerField()
 
