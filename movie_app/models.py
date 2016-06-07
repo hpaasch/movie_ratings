332bd698f8d6225f0a8bacac72575ecb39be74f8
hpaasch <hepaasch@gmail.com>
2016-06-07 13:40:44 -0400

    Initial commit.

---
+++ movie_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Movies(models.Model):
+    pass
+
+class Reviewers(models.Model):
+    pass
+
+class Ratings(models.Model):
+    pass
